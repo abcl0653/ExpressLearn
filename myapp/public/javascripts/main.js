7ce54e8437b9ff1564b89796b3457c288a7b207f
Charlie Cai <fd.cailin@outlook.com>
2016-05-02 19:50:33 +0800

    Express Initial

---
+++ myapp/public/javascripts/main.js
@@ -0,0 +1,59 @@
+window.addEventListener("hashchange", switch_sign, false);
+
+//$("#sign-form-1").validate();
+
+//$("#sign-form-2").validate();
+
+// Validation
+$("#sign-form-1").submit(function(event) {
+	
+	var fullname = $("input[name='fullname']").val();
+	if ( fullname.trim().length === 0) {
+
+		event.preventDefault();
+
+		var fullnameError = '<label id="fullnameError" class="error is-visible">请填写姓名</label>'
+		$("input[name='fullname']").next().remove(".error");
+		$("input[name='fullname']").after(fullnameError);
+	}
+
+	var account = $("input[name='account']").val();
+	if (account.trim().length === 0) {
+
+		event.preventDefault();
+
+		var accountError = '<label class="error is-visible">请填写邮箱或手机号</lable>';
+		$("input[name='account']").next().remove(".error");
+		$("input[name='account']").after(accountError);
+	}
+
+	
+	//alert("called!");
+	//console.log("Called!!");
+});
+
+$("input").focus(function(){
+	//console.log('testddd');
+  $(this).next().remove(".error");
+});
+
+//$("#sign-form-1").submit(function(event) {alert("alert called");});
+
+function switch_sign () {
+	//alert(window.location.hash);
+	if (window.location.hash === '#signin') {
+		$('a[href="\#signin"]').addClass('active');
+		$('a[href="\#signup"]').removeClass('active');
+		$('.navs-slider').attr('data-active-index','1');
+		$('.view-signin').show();
+		$('.view-signup').hide();
+	} else if (window.location.hash === '#signup') {
+		$('a[href="\#signin"]').removeClass('active');
+		$('a[href="\#signup"]').addClass('active');
+		$('.navs-slider').attr('data-active-index','0');
+		$('.view-signup').show();
+		$('.view-signin').hide();
+	}
+	
+
+}
